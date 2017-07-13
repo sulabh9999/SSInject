1ad2168be4e7b3f7d168662b700b72c3576d2d9e
sulabh qg <sulabh9999@gmail.com>
2017-07-13 17:36:41 +0530

    Initial Commit

---
+++ SSInject/SSInject.h
@@ -0,0 +1,19 @@
+//
+//  SSInject.h
+//  SSInject
+//
+//  Created by Sulabh on 13/07/17.
+//  Copyright © 2017 Sulabh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSInject.
+FOUNDATION_EXPORT double SSInjectVersionNumber;
+
+//! Project version string for SSInject.
+FOUNDATION_EXPORT const unsigned char SSInjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSInject/PublicHeader.h>
+
+
